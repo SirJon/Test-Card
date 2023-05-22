f0e322c24d5bc29107ed444a767ae5e440d684d9
Evgeny <ew.silin@gmail.com>
2023-05-22 11:05:35 +0300

    CSS

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-"use strict";
-(function () {
-  const canvas = document.getElementById('canvas');
-  const yellow = `rgb(250, 212, 47)`;
-  const green = `#71aa4a`;
-  const gray = `rgb(240, 240, 240)`;
-  const rad = (deg) => {
-    return -(deg * PI) / 180;
-  };
-  const arc = (ctx, start, end, color) => {
-    ctx.beginPath();
-    ctx.lineWidth = 6;
-    ctx.lineCap = `round`;
-    ctx.strokeStyle = `${color}`;
-    ctx.arc(75, 75, 72, rad(start), rad(end));
-    ctx.stroke();
+// function onChange(e) {
+//   console.log(e.target.value);
+// }
+
+// const debounce = (fn, ms) => {
+//   let timeout;
+//   return function () {
+//     const fnCall = () => { fn.apply(this, arguments) };
+
+//     clearTimeout(timeout);
+
+//     timeout = setTimeout(fnCall, ms);
+//   }
+// }
+
+// onChange = debounce(onChange, 200);
+
+// document.querySelector('input').addEventListener('keyup', onChange);
+
+
+function throttle(fn, ms) {
+  let isThrottled = false;
+  let saveArgs;
+  let saveThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      saveArgs = arguments;
+      saveThis = this;
+      return;
+    }
+    fn.apply(this, arguments);
+    isThrottled = true;
   }
-  const PI = Math.PI;
-  if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
 
-    arc(ctx, 205, 110, gray);
-    arc(ctx, 310, 205, green);
-    arc(ctx, 70, 310, yellow);
+  setTimeout(function () {
+    isThrottled = false;
+    if (saveArgs) {
+      wrapper.apply(saveThis, saveArgs);
+      saveArgs = null;
+      saveThis = null;
+    }
+  }, ms);
 
-  }
 
-  const widthGraf = ({ id, mainWidth, anotherWidth }) => {
-    const block = document.querySelector(`#${id}`);
-    const main = block.querySelector(`.js-graf--main`);
-    const another = block.querySelector(`.js-graf--another`);
-    main.style.width = `${mainWidth}%`;
-    another.style.width = `${anotherWidth}%`;
-  }
-
-  widthGraf({
-    id: `indicators-1`,
-    mainWidth: 32,
-    anotherWidth: 29,
-  });
-
-  widthGraf({
-    id: `indicators-2`,
-    mainWidth: 39,
-    anotherWidth: 37,
-  });
-
-  widthGraf({
-    id: `indicators-3`,
-    mainWidth: 25,
-    anotherWidth: 29,
-  });
-
-})();
+  return wrapper;
+}
